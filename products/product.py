57de5515791b56173d884430bbb8bb418229fd2e
gbonillo463 <spamgerard4@gmail.com>
2026-02-01 19:37:34 +0100

    module products created

--- products/product.py
+++ products/product.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
+from food_package import Wrapping, Bottle, Glass, Box
+from food_package import FoodPackage
 #Write your code here
 
 class Product(ABC):
-    def __init__(self,id:str,name:str,price:float):
-      self.id = id
-      self.name = name
-      self.price = price     
+    def __init__(self, id:str, name:str, price:float):
+        self.id = id
+        self.name = name
+        self.price = price     
     
     def describe(self):
         return f"Product - Type: {self.type()}, Name: {self.name}, Id: {self.id} , Price: {self.price} , {self.foodPackage().describe()}."   
@@ -14,11 +16,11 @@ class Product(ABC):
     def type(self) -> str:
         pass
     @abstractmethod
-    def foodPackage(self)->FoodPackage:
+    def foodPackage(self) -> FoodPackage:
         pass  
 
 class Hamburger(Product):
-    def __init__(self,id:str,name:str,price:float):
+    def __init__(self, id:str, name:str, price:float):
         super().__init__(id,name,price)
     def type(self) -> str:
         return "Hamburguesa"
@@ -26,13 +28,25 @@ class Hamburger(Product):
         return Wrapping()
         
 class Soda(Product):
-    #Write your code here
-    pass
+    def __init__(self, id:str, name:str, price:float):
+        super().__init__(id, name, price)
+    def type(self):
+        return 'Sprite'
+    def foodPackage(self):
+        return Bottle()
 
 class Drink(Product):
-    #Write your code here
-    pass
+    def __init__(self, id:str, name:str, price:float):
+        super().__init__(id, name, price)
+    def type(self):
+        return 'Coca-Cola'
+    def foodPackage(self):
+        return Glass()
 
 class HappyMeal(Product):
-    #Write your code here
-    pass
+    def __init__(self, id:str, name:str, price:float):
+        super().__init__(id, name, price)
+    def type(self):
+        return 'HappyMeal Menu'
+    def foodPackage(self):
+        return Box()
