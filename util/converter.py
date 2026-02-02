203b268e8d72c4100034532708728901fef1cb3a
gbonillo463 <spamgerard4@gmail.com>
2026-02-02 12:38:18 +0100

    Update converter.py

--- util/converter.py
+++ util/converter.py
@@ -1,23 +1,37 @@
 from abc import ABC, abstractmethod
-#Write your code here
+from .file_manager import *
+from products.product import Product
+from users.user import Customer, Cashier
+import pandas as pd
+
+
 
 class Converter(ABC):
-  @abstractmethod
-  def convert(self,dataFrame,*args) -> list:
-      pass  
-  def print(self, objects):
-    for item in objects:
-      print(item.describe())
+	@abstractmethod
+	def convert(self, dataFrame: pd.DataFrame, *args) -> list:
+		pass  
+	def print(self, objects):
+		for item in objects:
+			print(item.describe())
 
 class CashierConverter(Converter):
-  def convert(self,dataFrame):    
-    #Write your code here
-    pass
+	def convert(self, dataframe: pd.DataFrame):
+		cashier = Cashier()
+		cashiers = []
+		for index, row in dataframe.iterrows():
+			cashier.name = row['name']
+			cashier.dni = row['dni']
+			cashier.age = row['age']
+			cashier.timeTable = row['timetable']
+			cashier.salary = row['salary']
+			cashiers.append(cashier)
+		
+		return cashiers
 
 class CustomerConverter(Converter):
-  #Write your code here
-  pass
+	#Write your code here
+	pass
 
 class ProductConverter(Converter):
-  #Write your code here
-  pass
+	#Write your code here
+	pass
