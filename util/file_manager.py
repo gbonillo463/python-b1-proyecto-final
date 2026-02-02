9a4f48c7b7559e97eeaed8b1bac3370b781f71b5
gbonillo463 <spamgerard4@gmail.com>
2026-02-02 13:58:58 +0100

    created a product_type var, for ProductMap in converter.py

--- util/file_manager.py
+++ util/file_manager.py
@@ -1,11 +1,16 @@
 import pandas as pd
+from pathlib import Path
 
 class CSVFileManager:
 	def __init__(self, path: str):
 		self.path = path
 
 	def read(self):
-		return pd.read_csv(self.path)
-	
+		dataframe = pd.read_csv(self.path)
+		change_path = Path(self.path)
+		product_type = change_path.stem
+
+		return dataframe, product_type
+
 	def write(self, dataFrame: pd.DataFrame):
 		dataFrame.to_csv(self.path, index=False)
